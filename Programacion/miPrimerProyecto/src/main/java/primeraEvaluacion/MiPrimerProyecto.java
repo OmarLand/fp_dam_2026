773537e631a0bae1755f29254420ada11e0fdbbc
OmarjLand <omar.land2010@gmail.com>
2026-09-16 19:39:57 +0200

    Subiendo mi primer Proyecto de Estudios

---
+++ Programacion/miPrimerProyecto/src/main/java/primeraEvaluacion/MiPrimerProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package primeraEvaluacion;
+
+/**
+ *
+ * @author OmarjLand
+ */
+public class MiPrimerProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Programacion con JAVA - DAM 2026");
+    }
+}
